c67dd3daacf134222c32ce5dc03d05561dace2ee
melisalsan <melis.alsan@hisarschool.k12.tr>
2023-08-31 11:31:41 +0300

    deneme

---
+++ Hello.java
@@ -0,0 +1,14 @@
+// First 10 lines of code .)
+public class Hello {
+    public static void main(String[] args) {
+        String mel; // declaring var mel
+        mel = "melso";// assigning a value
+        String course = "AP CS A";// declaring a var & assigning a value
+        String y = "Hello";
+        System.out.println(y + course + "! This is " + mel + ". Nice to meet you .)"); //printing lol
+        System.out.println("What else...");
+        System.out.println("How's everybody?");
+    }
+}
+
+
